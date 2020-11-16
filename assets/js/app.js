7e436936e7717b4abde586e037835489d23efc37
Yimura <andreas.maerten@scarlet.be>
2020-11-16 02:32:04 +0100

    JS: Set HeroApp to default to the Hero page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,34 @@
+import Hero from './hero.js'
 import HeroAPI from './heroapi.js'
+import Router from './router.js'
 
 export default class HeroAPP {
-    constructor() {
+    _ = false;
+    router = new Router(this);
 
+    constructor() {
+        this._loadLanding();
+
+        this.router.registerClassForPath('/hero', new Hero());
+    }
+
+    async _loadLanding() {
+        await this.router.prepare('/hero');
+
+        this._ready();
+    }
+
+    /**
+     * @private
+     */
+    _ready() {
+        if (this._) this.router.navigate('/hero');
+        this._ = true;
     }
 
     domLookup(e) {
+        this.content = document.querySelector('.js-content');
 
+        this._ready();
     }
 }
